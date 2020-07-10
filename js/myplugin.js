370ca30c983793d2d619762136fc254d30b4d7d4
Peter-95 <57274805+Peter-95@users.noreply.github.com>
2020-07-10 20:51:13 +0200

    Add files via upload

---
+++ js/myplugin.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+    // navbar  dynamic background
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= 500) {
+            $('.navbar').css('background', '#2B2B2B');
+            $('.navbar-toggler').css('hover', '#FFF');
+        } else {
+            $('.navbar').css('background', 'transparent');
+        }
+    });
+
+
+    // body padding with navbar
+    $('body').css('paddingTop', $('.navbar').innerHeight() + 10);
+
+
+    // navbar active color
+    $('.navbar li a').click(function (e) {
+        $('.navbar a').removeClass('active');
+        $(this).addClass('active');
+    });
+
+
+    // syc navbar with section
+    $(window).scroll(function () {
+        $(secation).each(function () {
+            if ($(window).scrollTop() > $(this).offset().top) {
+                var blockID = $(this).arrt('id');
+                $('.navbar a').removeClass('active');
+                $('.navbar li a[data-scroll="' + blockID + '"]').addClass('active');
+            }
+        });
+    });
+
+
+    // secation scroll smoothly
+    $('.navbar li a').click(function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('#' + $(this).data('scroll')).offset().top + 1
+        }, 1000);
+    });
+
+
+    // feedback button
+    $('#btn-feadback').click(function () {
+        $('#feedback-form').fadeToggle('slow');
+    });
+
+
+    // scrollup btn
+    $(window).scroll(function () {
+
+        if ($(window).scrollTop() >= 1000) {
+            $('.scrollup-btn').fadeIn(500);
+        } else {
+            $('.scrollup-btn').fadeOut(500);
+        }
+    });
+    $('.scrollup-btn').click(function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+});
